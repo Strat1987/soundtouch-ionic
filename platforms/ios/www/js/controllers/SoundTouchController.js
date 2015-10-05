9263a75be6541b6f05f9f8f074a5a53eda9fb082
michaelgeerts <geertsmichael@gmail.com>
2015-10-05 13:25:41 +0200

    Added Getting volume

--- platforms/ios/www/js/controllers/SoundTouchController.js
+++ platforms/ios/www/js/controllers/SoundTouchController.js
@@ -7,11 +7,16 @@ angular.module('SoundTouchHack.controller.SoundTouchController', ['SoundTouchHac
     $scope.device = $localStorage.device;
 
     if (typeof $scope.device !== 'undefined') {
+      $scope.device.volume = 20;
       $scope.device.volume = SoundtouchAPI.getVolume($scope.device);
       console.log('SoundtouchAPI getVolume: ' + $scope.volume);
 
     }
   });
+
+    $scope.getVol = function() {
+      $scope.device.volume = SoundtouchAPI.getVolume($scope.device)
+    };
 
   $scope.volumeChanged = function() {
     console.log('Volume has changed: ' + $scope.device.volume);
